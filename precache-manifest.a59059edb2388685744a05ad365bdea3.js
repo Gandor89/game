06a797881f2c6db45a662e02fb64922cd1e042ac
VladimirLjamic <ljamic@gmail.com>
2019-09-19 22:41:40 +0200

    Updates

---
+++ precache-manifest.a59059edb2388685744a05ad365bdea3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e43b2f6ca29df592e61739269c1bd35",
+    "url": "/game/index.html"
+  },
+  {
+    "revision": "a836b4670ab276c58a3f",
+    "url": "/game/static/css/main.6123cfb5.chunk.css"
+  },
+  {
+    "revision": "6b6c46536e1ab5e33fbe",
+    "url": "/game/static/js/2.a55bf63b.chunk.js"
+  },
+  {
+    "revision": "a836b4670ab276c58a3f",
+    "url": "/game/static/js/main.abd5243a.chunk.js"
+  },
+  {
+    "revision": "6deac6a25242a4c03ed2",
+    "url": "/game/static/js/runtime~main.3f4f068e.js"
+  }
+]);
